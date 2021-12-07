b02275138b7eb0dc0e9bfb19c2a12d5bdd271269
Ze8art <zsapoznik@gmail.com>
2021-12-07 17:36:59 +0200

    ClassPoint

---
+++ ClassPoint/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Point {
+	private:
+		int x;
+		int _y;
+	public:
+		Point() {
+			cout << "Point(" << x << ", " << _y << ") constructed" << endl;
+		};
+		Point(int x, int y) {
+			this->x = x;
+			_y = y;
+			cout << "Point(" << x << ", " << _y << ") constructed" << endl;
+		}
+		~Point() {
+			cout << "Point(" << x << ", " << _y << ") destroyed" << endl;
+		}
+		int getX() { return x; }
+		int getY() { return _y; }
+		void setX(int x) { this->x = x; }
+		void setY(int y) { _y = y; }
+		void toString() {
+			cout << "(" << x << ", " << _y << ")" << endl;
+		}
+};
+
+
+int main() {
+	Point A(1, 2);
+	Point C;
+	C = A;
+	C.toString();
+	A.toString();
+	A.setX(10);
+	A.setY(20);
+	cout << A.getX() << endl;
+	cout << A.getY() << endl;
+	A.toString();
+}
